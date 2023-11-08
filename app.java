2a09019d759d81c25c95fc664eef49ae7429120c
varunm02 <110086993+varunm02@users.noreply.github.com>
2023-11-08 19:05:44 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,27 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WebOutput {
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new HelloWorldHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class HelloWorldHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Sample Text Output on Web Page";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
